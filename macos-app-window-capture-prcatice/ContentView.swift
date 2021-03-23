500302690a0ce907049c111164790a3bb51581c4
soudegesu <soudegesu@hotmail.co.jp>
2021-03-23 23:20:13 +0900

    initial commit

---
+++ macos-app-window-capture-prcatice/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macos-app-window-capture-prcatice
+//
+//  Created by Takaaki Suzuki on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macos-app-window-capture-prcaticeTests/macos_app_window_capture_prcaticeTests.swift
@@ -0,0 +1,33 @@
+//
+//  macos_app_window_capture_prcaticeTests.swift
+//  macos-app-window-capture-prcaticeTests
+//
+//  Created by Takaaki Suzuki on 2021/03/23.
+//
+
+import XCTest
+@testable import macos_app_window_capture_prcatice
+
+class macos_app_window_capture_prcaticeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
